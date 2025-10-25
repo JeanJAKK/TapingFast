aafd9e0d3d5d00a0689d9a9b7bfce8be0b80b7e5
merveille228 <merveilleamevigbe2@gmail.com>
2025-10-25 09:37:01 +0000

    voici ma modif

--- tapingFast/script.js
+++ tapingFast/script.js
@@ -1,7 +1,11 @@
 let temps = 60
+let zero = 0
 const compteur = document.getElementById("time")
 let secondeRestante = setInterval(() => {
-    temps --
-    compteur.textContent = temps
+    temps --;
+    compteur.textContent = temps;
+    if (temps < 0) {
+        compteur.textContent = zero;
+    }
 }, 1000);
 
