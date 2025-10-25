c445ca576db1db897cfeeba5540005ff2330cad2
merveille228 <merveilleamevigbe2@gmail.com>
2025-10-25 11:55:09 +0000

    une autre mise a jour

--- tapingFast/script.js
+++ tapingFast/script.js
@@ -1,5 +1,10 @@
 let temps = 60
 let zero = 0
+
+const titre = document.querySelector("h1");
+const paragraphe = document.querySelector(".body_p")
+const btn = document.querySelector(".box")
+
 const compteur = document.getElementById("time")
 let secondeRestante = setInterval(() => {
     temps --;
@@ -9,3 +14,9 @@ let secondeRestante = setInterval(() => {
     }
 }, 1000);
 
+// animation au chargement de la page
+window.addEventListener('load', () => {
+    titre.classList.add("show_titre");
+    paragraphe.classList.add("show_p");
+    btn.classList.add("show_btn")
+})
